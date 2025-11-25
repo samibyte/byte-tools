23c2104ab1d992f45c671b454ecd08dfc6053504
samibyte <samibyte.web@gmail.com>
2025-11-25 11:41:23 +0600

    feat: add navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Navbar05 } from "@/components/ui/shadcn-io/navbar-05";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +28,13 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        {/* Header */}
+        <Navbar05 />
+        {/* Main  */}
+        <main>{children}</main>
+
+        {/* Footer */}
+        <footer></footer>
       </body>
     </html>
   );
